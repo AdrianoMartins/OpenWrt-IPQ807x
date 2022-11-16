f3f66c7aedfc8c574fa009c1864d3e857df8cd0a
Adriano Martins <adrianomartins@gmail.com>
2022-11-16 03:17:01 +0000

    Cleanup Packages

--- diy-mod.sh
+++ diy-mod.sh
@@ -4,15 +4,15 @@
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
 # Packages
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
 
 # Packages Repo
-git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
-shopt -s extglob
+#git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+#shopt -s extglob
 
-PACKAGES="("adguardhome"|"luci-app-adguardhome"|"luci-app-cpufreq"|"luci-app-argon-config"|"luci-app-openvpn-server"|"openwrt_oscam"|"luci-app-oscam"|"qBittorrent"|"luci-app-qbittorrent"|"rblibtorrent"|"qBittorrent-Enhanced-Edition"|"qtbase"|"qttools"|"luci-app-rclone"|"luci-app-zerotier"|"luci-app-wizard"|"vsftpd-alt"|"luci-app-vsftpd"|"luci-theme-argon"|"luci-theme-edge")"
-rm -R -f package/openwrt-packages/!$PACKAGES
+#PACKAGES="("adguardhome"|"luci-app-adguardhome"|"luci-app-cpufreq"|"luci-app-argon-config"|"luci-app-openvpn-server"|"openwrt_oscam"|"luci-app-oscam"|"qBittorrent"|"luci-app-qbittorrent"|"rblibtorrent"|"qBittorrent-Enhanced-Edition"|"qtbase"|"qttools"|"luci-app-rclone"|"luci-app-zerotier"|"luci-app-wizard"|"vsftpd-alt"|"luci-app-vsftpd"|"luci-theme-argon"|"luci-theme-edge")"
+#rm -R -f package/openwrt-packages/!$PACKAGES
 
 # Modify NTP Server 
 sed -i "s/0.openwrt.pool.ntp.org/pool.ntp.org/g" package/base-files/files/bin/config_generate
